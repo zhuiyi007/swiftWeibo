4e561f27d9eada9ff0a28e1ab1d08915106c6c38
zhangsen <zhangsen@yongche.com>
2015-07-14 15:18:15 +0800

    refs #34800, 搭建整体框架的文件

---
+++ weibo/Classes/Module/Main/MainTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarController.swift
+//  weibo
+//
+//  Created by ZhuiYi on 15/7/14.
+//  Copyright © 2015年 ZhuiYi. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
